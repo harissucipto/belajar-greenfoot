21c9906c08d3fec3f35c48f1a5d3a12565591c3c
Haris Sucipto <harisssucipto@gmail.com>
2018-01-02 09:06:22 +0700

    tambah latihan game avoiderWorld

---
+++ AvoiderGameExperimentation/AvoiderWorld.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AvoiderWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AvoiderWorld extends World
+{
+
+    /**
+     * Constructor for objects of class AvoiderWorld.
+     * 
+     */
+    public AvoiderWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // set flase when world is unbounding (kondisi dimana actor bisa melewati pembatas screen)
+        super(600, 400, 1, false); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Avatar avatar = new Avatar();
+        addObject(avatar,301,204);
+    }
+    
+    /**
+     * Act when game is started.
+     * 
+   */
+  public void act() {
+      // Randomly add enimies to the world
+      if (Greenfoot.getRandomNumber(1000) < 20) { // berapa persen musuh keluar ke screen
+          Enemy e = new Enemy();
+          addObject(e, Greenfoot.getRandomNumber(getWidth() - 20) + 10, -30);
+          
+      }
+  }
+    
+}
---
+++ AvoiderGameExperimentation/Enemy.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Actor
+{
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        setLocation(getX(), getY() + 1); // ketika musuh berhasil dibuat maka musuh bergerak terus turn 1 pixel ke bawah (y)
+        
+        // cek hapus dan menghapus apabila object telah keluar screen
+        checkRemove();
+    }    
+    
+    public void checkRemove() {
+        World w = getWorld();
+        if ( getY() > w.getHeight() + 30) { // ketika object menyentuh scrren (+ 30 opsi karena object ditambahkan -30 pada world)
+            w.removeObject(this);
+        }
+    }
+        
+}
